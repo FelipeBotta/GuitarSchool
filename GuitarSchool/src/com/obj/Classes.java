00755c3d992802462140e793e9d6ac6a1931c21c
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-08-11 17:19:52 -0300

    Update Classes.java

--- GuitarSchool/src/com/obj/Classes.java
+++ GuitarSchool/src/com/obj/Classes.java
@@ -3,5 +3,19 @@ package com.obj;
 public class Classes {
 	
 	int classes;
+	
+	
+	private void addClass() {
+		
+	for(int i = 0; i < classes; i++) {
+		classes++;
+		System.out.println("Você concluiu mais uma aula");
+	}
+	
+	}
+	
+	private void infoClass() {
+		System.out.println("");
+	}
 
 }
